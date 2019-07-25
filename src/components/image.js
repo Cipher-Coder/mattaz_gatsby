249b9e11ad59918384e68b3293a84c2739d931b4
Cipher-Coder <matthew.rebehn@gmail.com>
2019-07-25 15:35:29 -0400

    added picture for index | changed some css

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "masthead2.jpg" }) {
+      placeholderImage: file(relativePath: { eq: "computerVector.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
