6dfdd705e154bfb5f0ebcb54bfe836953568f099
Cipher-Coder <matthew.rebehn@gmail.com>
2019-08-08 13:14:24 -0400

    added Layout & SEO components to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,25 +1,31 @@
 import React from "react"
 import Link from "gatsby-link"
 import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const BlogPage = ({ data }) => (
-  <div>
-    <h1>Latest Posts</h1>
-    {data.allMarkdownRemark.edges.map(post => (
-      <div key={post.node.id}>
-        <h3>{post.node.frontmatter.title}</h3>
-        <small>
-          Posted {post.node.frontmatter.author} on {post.node.frontmatter.date}
-        </small>
-        <br />
-        <br />
-        <Link to={post.node.frontmatter.path}>Read More...</Link>
-        <br />
-        <br />
-        <hr />
-      </div>
-    ))}
-  </div>
+  <Layout>
+    <SEO title="Mattaz Blog" />
+    <div>
+      <h1>Latest Posts</h1>
+      {data.allMarkdownRemark.edges.map(post => (
+        <div key={post.node.id}>
+          <h3>{post.node.frontmatter.title}</h3>
+          <small>
+            Posted {post.node.frontmatter.author} on{" "}
+            {post.node.frontmatter.date}
+          </small>
+          <br />
+          <br />
+          <Link to={post.node.frontmatter.path}>Read More...</Link>
+          <br />
+          <br />
+          <hr />
+        </div>
+      ))}
+    </div>
+  </Layout>
 )
 
 export const pageQuery = graphql`
