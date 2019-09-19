f9957b3136a546cd0a7aab5cc98a6adce21a13c8
Cipher-Coder <matthew.rebehn@gmail.com>
2019-09-19 14:32:55 -0400

    changing contact page | taking out zoho contact and adding my own

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,43 +3,70 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "../styles/contact.css"
 
 const ContactPage = () => (
   <Layout>
     <SEO title="Contact Us" />
-    <h1 className="text-center pt-5" style={{ color: `rgb(59, 153, 224)` }}>
-      Get in Touch!!
-    </h1>
-    <p className="text-center col-sm-4 mx-auto" style={{ opacity: 0.8 }}>
+    <h1 className="text-center pt-5 contact-title">Get in Touch!!</h1>
+    <p className="text-center col-sm-4 mx-auto contact-desc">
       For any further information or to get a quote please feel free to reach
       out anytime. We look forward to hearing from you and will get back to you
       as soon as possible.
     </p>
-    <div
-      className="col-sm-6"
-      style={{
-        display: "block",
-        marginLeft: "auto",
-        marginRight: "auto",
-        paddingBottom: "20px",
-      }}
-    >
-      <iframe
-        title="Contact Form"
-        src="https://forms.zohopublic.com/matthew22/form/MattazWebDevelopment/formperma/w2loiSb_5UWebQDppVGzUSMWZW8ZxwHlFD89gRckIJE"
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          display: `block`,
-          height: `850px`,
-          width: `80%`,
-          border: `none`,
-        }}
-      />
+    <div class="container">
+      <div className="row contact-form">
+        <form method="post" action="/">
+          <h1 class="form-title mt-3">Mattaz Web Development</h1>
+          <h4 class="form-title mb-5">Have a question or want to chat?</h4>
+          <div>
+            <label for="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter Name *"
+              required="required"
+            />
+          </div>
+          <div>
+            <label for="subject">Subject:</label>
+            <select name="subject" id="subject">
+              <option value="quote">Product Quote</option>
+              <option value="inquiry">General Inquiry</option>
+              <option value="help">Help</option>
+              <option value="bug">Report a Bug</option>
+              <option value="devTabs">Dev Tabs Info</option>
+            </select>
+          </div>
+          <div>
+            <label for="mail">E-mail:</label>
+            <input
+              type="email"
+              id="mail"
+              name="email"
+              placeholder="Enter Email *"
+              required="required"
+            />
+          </div>
+          <div>
+            <label for="msg">Message:</label>
+            <textarea
+              id="msg"
+              name="message"
+              placeholder="Please Write Message Here *"
+              required="required"
+            ></textarea>
+          </div>
+          <div class="send-button">
+            <button class="btn btn-primary mb-5">Send</button>
+          </div>
+        </form>
+      </div>
     </div>
     <Link
       to="/"
-      style={{ display: `block`, textAlign: `center`, paddingBottom: `20px` }}
+      style={{ display: `block`, textAlign: `center`, paddingBottom: `30px` }}
     >
       Go back to the Homepage
     </Link>
