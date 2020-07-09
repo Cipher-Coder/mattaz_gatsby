c49d75aa3852dd9bcbfb98c68070f90b4a09bb1f
Matt <matthew.rebehn@gmail.com>
2020-07-09 17:34:48 -0400

    made the contact page have its own css so i could include media queries and make the form render larger on smaller screens and some other things

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,53 +2,31 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "../styles/contact.css"
 
 const ContactPage = () => {
   return (
     <Layout>
       <SEO title="Contact Us" />
-      <div
-        className="container-fluid"
-        style={{ backgroundColor: `rgba(218, 218, 218, 0.651)` }}>
-        <h1
-          className="text-center pt-5 contact-title"
-          style={{ color: `rgb(59, 153, 224)` }}>
-          Get in Touch!!
-        </h1>
-        <p
-          className="text-center col-sm-4 mx-auto contact-desc"
-          style={{ opacity: `0.8` }}>
+      <div className="container-fluid main-div">
+        <h1 className="text-center pt-5 contact-title">Get in Touch!!</h1>
+        <p className="text-center col-sm-8 col-md-6 col-lg-4 mx-auto contact-desc">
           For any further information, or to get a quote, please feel free to
           reach out anytime. We do excellent freelance work at very affordable
           prices! We look forward to hearing from you, and will get back to you
           as soon as possible.
         </p>
         <div className="container">
-          <div
-            className="row contact-form"
-            style={{ margin: `35px auto 40px` }}>
+          <div className="row contact-form">
             <iframe
+              className="contact-iframe"
               scrolling="no"
               title="Contact Form"
               src="https://forms.zohopublic.com/matthew22/form/MattazWebDevelopment/formperma/w2loiSb_5UWebQDppVGzUSMWZW8ZxwHlFD89gRckIJE"
-              style={{
-                marginLeft: `auto`,
-                marginRight: `auto`,
-                display: `block`,
-                height: `850px`,
-                width: `70%`,
-                border: `none`,
-              }}
             />
           </div>
         </div>
-        <Link
-          to="/"
-          style={{
-            display: `block`,
-            textAlign: `center`,
-            paddingBottom: `30px`,
-          }}>
+        <Link className="link-home" to="/">
           Go back to the Homepage{" "}
         </Link>{" "}
       </div>
