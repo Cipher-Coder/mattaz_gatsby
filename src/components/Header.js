6e9e0acfa3aeed3a903e45d971363c85be4a9e6b
Matt <matthew.rebehn@gmail.com>
2020-08-12 21:17:28 -0400

    took out some code that was not needed in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import logo from "../images/logo55.png"
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
     <Navbar.Brand href="https://mattaz.com" style={{ color: `#4c9fd8` }}>
       <img src={logo} style={{ maxHeight: `40px` }} alt="Mattaz Logo" />
@@ -13,7 +12,7 @@ const Header = ({ siteTitle }) => (
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="mr-auto"/>
+      <Nav className="mr-auto" />
       <Nav>
         <Link
           to="/"
@@ -59,13 +58,5 @@ const Header = ({ siteTitle }) => (
     </Navbar.Collapse>
   </Navbar>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
