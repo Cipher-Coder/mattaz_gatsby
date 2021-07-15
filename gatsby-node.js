e84aba0a80f9c653fcf57631872c08a3c3243c54
Matthew <matthew.rebehn@gmail.com>
2021-07-14 22:49:19 -0400

    Updated prettier and node

--- gatsby-node.js
+++ gatsby-node.js
@@ -25,7 +25,7 @@ exports.createPages = ({ actions, graphql }) => {
         }
       }
     }
-  `).then(res => {
+  `).then((res) => {
     if (res.errors) {
       return Promise.reject(res.errors)
     }
